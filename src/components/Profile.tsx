cb82ffdff0853c2d2c0615415e1a6adcda60765d
Erikstellet <erikstellet@gmail.com>
2021-02-23 10:31:16 -0300

    NLW#4 | Trilha ReactJS - Aula 2 Part 1

---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile()
+{
+    return (
+        <div className={ styles.profileContainer }>
+            <img src="https://github.com/Erikstellet.png" alt=""/>
+
+            <div>
+                <strong>Erik Stellet</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
